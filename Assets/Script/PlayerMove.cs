59266cfde2623c91e9f077a53de4fa43254a9ecb
Horiuchi Fubuki <fubuki199914@gmail.com>
2020-09-15 03:12:52 +0900

    初up

---
+++ Assets/Script/PlayerMove.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    PlayerParamClass paramClass = PlayerParamClass.GetInstance();
+
+    //加速減速
+    [SerializeField, Header("加速値")]
+    private float accele;
+    [SerializeField, Header("減速値"), Tooltip("0:通常 1:上り坂 2:下り坂")]
+    private float[] decele;
+    private sbyte setDecele;　
+
+    private void Start()
+    {
+        SetDecele(0);
+    }
+    void Update()
+    {
+        //加速入力
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+            paramClass.SpeedFluctuation(accele);
+        //減速(毎秒)
+
+        paramClass.SpeedFluctuation(decele[setDecele]);
+
+        ActionPlayerMove();
+    }
+    private void ActionPlayerMove()
+    {
+        Vector3 movePos = transform.position;
+        movePos.z += paramClass.playerSpeed;
+        transform.position = movePos;
+    }
+    //選んだ減速値をセットする
+    private void SetDecele(sbyte num)
+    {
+        setDecele = num;
+    }
+}
---
+++ Assets/Script/PlayerParamClass.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// プレイヤーのパラメーター管理クラス
+/// </summary>
+public class PlayerParamClass
+{
+    private static PlayerParamClass playerParamClass;
+
+    /// <summary>
+    /// コンストラクタ（初期化）
+    /// </summary>
+    public PlayerParamClass()
+    {
+        playerLife = 3;
+        playerSpeed = 0;
+    }
+
+    /// <summary>
+    /// PlayerParamClassをシングルトンパターンでインスタンス化
+    /// </summary>
+    /// <returns></returns>
+    public static PlayerParamClass GetInstance()
+    {
+        //singleton
+        if (playerParamClass == null)
+        {
+            playerParamClass = new PlayerParamClass();
+        }
+        return playerParamClass;
+    }
+
+    public sbyte
+        playerLife { get; private set; }
+    const sbyte MaxLife = 3;
+    public float
+        playerSpeed{ get; private set; }
+
+    /// <summary>
+    /// プレイヤーのライフ変動を格納
+    /// </summary>
+    /// <param name="val">変動する値</param>
+    public void LifeFluctuation(sbyte val)
+    {
+        playerLife += val;
+        if (playerLife > MaxLife)
+            playerLife = MaxLife;
+        //ライフがなくなったらGameOverへ遷移
+        if (playerLife <= 0)
+            playerDie();
+    }
+    private void playerDie()
+    {
+        Debug.Log("GameOver");
+    }
+
+    /// <summary>
+    /// プレイヤーの速度変動を格納
+    /// </summary>
+    /// <param name="val">変動する値</param>
+    public void SpeedFluctuation(float val)
+    {
+        playerSpeed += val;
+        if (playerSpeed < 0)
+            playerSpeed = 0;
+    }
+}

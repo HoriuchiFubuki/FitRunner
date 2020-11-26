7a7b9654aaba86f9b3a84e6d72e32274ba85a53f
nakano <nakandows@gmail.com>
2020-11-26 11:51:44 +0900

    仲野製ポンコツUI群
    ～冬の香りを乗せて～

---
+++ Assets/Script/Nakano/Result_Scene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Result_Scene : MonoBehaviour
+{
+    [SerializeField]
+    string nextScene = "NewScene";
+
+    private float timer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= 3)
+        {
+            SceneManager.LoadScene(nextScene);
+        }
+    }
+}

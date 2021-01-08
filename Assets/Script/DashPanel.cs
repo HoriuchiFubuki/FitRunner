1f0f899a1a3de354670a1aeb20f4ac7e70d20da3
hekocanbee <hekocanbee@gmail.com>
2021-01-08 18:26:24 +0900

    Add Dash Panel
    ダッシュパネル(仮)の追加

---
+++ Assets/Script/DashPanel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DashPanel : MonoBehaviour
+{
+    PlayerParamClass
+     paramClass = PlayerParamClass.GetInstance();
+
+    [Range(0.0f, 25.0f)]
+    public float
+        PanelSpeed = 1.0f;
+
+    private bool OnPanel = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OnPanel)
+            paramClass.SpeedFluctuation(PanelSpeed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "DashPanel")
+        {
+            OnPanel = true;
+            Debug.Log("aaaa");
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "DashPanel")
+        {
+            OnPanel = false;
+        }
+    }
+
+}
